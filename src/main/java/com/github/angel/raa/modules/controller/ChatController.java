c392a385e7e7230d6d1f648ea108e0c66929fb54
211250111 <211250111@smail.nju.edu.cn>
2023-11-29 11:19:53 +0800

    修改chat

--- src/main/java/com/github/angel/raa/modules/controller/ChatController.java
+++ src/main/java/com/github/angel/raa/modules/controller/ChatController.java
@@ -3,25 +3,21 @@ package com.github.angel.raa.modules.controller;
 import com.github.angel.raa.modules.dto.ChatDTO;
 import com.github.angel.raa.modules.service.interfaces.ChatService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
-@RequestMapping("/api")
+@Validated
 @RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("/chat")
 public class ChatController {
-
     private final ChatService chatService;
-
-    @Autowired
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-    }
 
     @PostMapping("/ask-question")
     public ResponseEntity<ChatDTO> askQuestion(@Valid @RequestBody ChatDTO chatDTO) {
-        ChatDTO response = chatService.generateAnswer(chatDTO);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(chatService.generateAnswer(chatDTO));
     }
 }
